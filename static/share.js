12ef16e39168b3f649c7af6ab235ddd5c83aa675
CyriloMark <cyrilo.mark@gmail.com>
2024-01-04 12:11:02 +0100

    static website

---
+++ static/share.js
@@ -0,0 +1,62 @@
+function getContent() {
+    const searches = document.location.search
+        .split("?")
+        .filter(a => a.length !== 0);
+
+    if (
+        searches.length !== 2 ||
+        !(searches[0] === "t=p" || searches[0] === "t=e")
+    )
+        return;
+
+    const type = searches[0];
+    const id = searches[1].substring(3);
+
+    fetch("https://kostrjanc.ew.r.appspot.com/share/recieve_content", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: id,
+            type: type == "t=p" ? 0 : 1,
+        }),
+    })
+        .then(rsp =>
+            rsp.json().then(data => setData(type == "t=p" ? 0 : 1, data))
+        )
+        .catch(e => console.log(e));
+}
+
+function setData(type, data) {
+    const container = document.getElementById("container");
+    if (data.isBanned) return;
+
+    container.innerHTML += `
+        <div class="header">
+            <img src="${data.creator.pic}" alt="profilny wobraz" />
+            <p>${data.creator.name}</p>
+        </div>
+        <div class="body">
+            <h2>${data.title}</h2>
+    `;
+
+    if (type === 0)
+        container.innerHTML += `
+            <img class="image" src="${data.img}" />
+        `;
+    else
+        container.innerHTML += `
+            <div class="map-box">
+                <p>Karta dawa jenož w kostrjanc App!</p>
+            </div>
+        `;
+
+    container.innerHTML += `
+        <div class="text-box">
+            <p>${data.description}</p>
+            <p>Wšitke informacije namakaš w kostrjanc App!</p>
+        </div>
+        </div>
+    `;
+}
